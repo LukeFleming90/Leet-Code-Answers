44ed6ead9fc411a2e9502b833f99d39bb25096bb
Luke Fleming <lukefleming@lukes-mbp.myfiosgateway.com>
2022-06-16 09:51:50 -0400

    Problem 1 Solved

---
+++ 1. Two Sum.js
@@ -0,0 +1,30 @@
+var twoSum = function(nums, target) {
+    
+    let compareObj = {};
+    
+    for(let i = 0; i < nums.length; i++) {
+        let currentNumber = nums[i]
+        compareObj[currentNumber] = i
+    }
+    
+    for(let i = 0; i < nums.length; i++) {
+        let diffNum = target - nums[i]
+        if(compareObj[diffNum]) {
+            return [compareObj[diffNum],i]
+        }
+    }
+    
+    return [0,0]
+};
+
+// Expected Result [0,1]
+
+console.log(twoSum([2,7,11,15],9))
+
+// Expected Result [1,2]
+
+console.log(twoSum([3,2,4],6))
+
+// Expected Result [0,1]
+
+console.log(twoSum([3,3],6))
